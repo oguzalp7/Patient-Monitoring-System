c456d8943cbccff9b10072b26272d3b7aa71d3a5
Oguz Alp Saglam <42783655+oguzalp7@users.noreply.github.com>
2020-12-06 15:24:21 +0300

    Update path_follower_basics.py

--- path_follower_basics.py
+++ path_follower_basics.py
@@ -1,52 +1,36 @@
 #!/usr/bin/env python
+
+# importing modules
 import roslib
 import sys
 import rospy
 import cv2
 import numpy as np
-from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import Twist
+
+# ROS Message Protocols
+from cv_bridge import CvBridge, CvBridgeError 
 from sensor_msgs.msg import Image
 
-class PathFollower(object):
+# Camera Listener Class
+class PmsCamera(object):
 	def __init__(self):
-		self.bridge_object = CvBridge()
-		self.image_sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.camera_callback)
-		"""self.velocity_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-		self.twist = Twist()"""
+		self.bridge_object = CvBridge()  # Creating CvBridge instance 
+		self.image_sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.camera_callback) # Subscriber to Camera Payload that publishes from socket
+	# Subscriber Callback
 	def camera_callback(self, data):
 		try:
-			cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding='bgr8')
-			"""height, width = cv_image.shape[:2]
-			descentre = 160
-			rows_to_watch = 20
-			crop_img = cv_image[int(height/4):height, 0: width]
-			hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-			lower_hsv = np.array([56, 115, 165])
-			higher_hsv = np.array([132, 255, 216])
-			mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
-			res = cv2.bitwise_and(crop_img, crop_img, mask=mask)
-			m = cv2.moments(mask, False)
-			try:
-				cx, cy = m['m10']/m['m00'], m['m01']/m['m00']
-			except ZeroDivisionError:
-				cy, cx = height/2, width/2
-			cv2.circle(res, (int(cx), int(cy)), 10, (0, 0, 255), -1)
-			error = cx - width/2
-			self.twist.linear.x = 0.2
-			self.twist.angular.z = -float(error)/100
-			self.velocity_pub.publish(self.twist)"""
+			cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding='bgr8')  # The image object that is the same thing with regular opencv projects.
+			# only difference is that we're receiving the camera payload from another computers camera.
 		except CvBridgeError as e:
 			print(e)
-		cv2.imshow('Surveillance Window', cv_image)
+		cv2.imshow('Surveillance Window', cv_image)  # dislay the result.
 		cv2.waitKey(3)
 
 def main():
-	rospy.init_node('path_following_node', anonymous=True)
-	path_follower_object = PathFollower()
-	
+	rospy.init_node('patient_surveillance_node', anonymous=True)  # node initialization
+	camera_listener_object = PmsCamera()  # camera listener object
 	try:
-		rospy.spin()
+		rospy.spin()  # run the Subscriber callback
 	except KeyboardInterrupt:
 		print('Shutting down')
 
