e8bc7d3bb702e76f606a0d4bec02a29b8cd6387f
Oguz Alp Saglam <42783655+oguzalp7@users.noreply.github.com>
2020-12-06 15:16:40 +0300

    Update iot_ventilation.ino

--- iot_ventilation.ino
+++ iot_ventilation.ino
@@ -1,15 +1,24 @@
-/* 
- * rosserial Subscriber Example
- * Blinks an LED on callback
- */
+/*
+* Author: Oguz Alp Saglam
+* Fake Ventilator
+*/
+//including libraries and ROS message headers
 #include<L298N.h>
 #include <ros.h>
 #include <std_msgs/Int32.h>
+
+//defining motordriver pins
 const unsigned int IN1 = 12;
 const unsigned int IN2 = 11;
 const unsigned int EN = 10;
-ros::NodeHandle  nh;
-L298N motor(EN, IN1, IN2);
+ros::NodeHandle  nh;  //creating ROS instance
+L298N motor(EN, IN1, IN2); //creating 1 motor instance
+
+//ROS Subscriber Callback
+/*
+* PARAMS: ROS Int32 type message instance
+* The motor driving algorithms runs here
+*/
 void messageCb( const std_msgs::Int32& toggle_msg){
   while(toggle_msg.data == 1){
     motor.setSpeed(255);
@@ -23,16 +32,18 @@ void messageCb( const std_msgs::Int32& toggle_msg){
   }
 }
 
+// ROS Subscriber instance
 ros::Subscriber<std_msgs::Int32> sub("/ventilation", &messageCb );
 
 void setup()
 { 
-  nh.initNode();
-  nh.subscribe(sub);
+  nh.initNode(); //ROS Node initialization
+  nh.subscribe(sub); //ROS Subscriber
 }
 
 void loop()
 {  
+  //Run the code
   nh.spinOnce();
   delay(1);
 }
